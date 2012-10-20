e74342a18ac2871c30807a218586c57545d42e06
mawanjin <mwj7999@hotmail.com>
2012-10-20 14:28:56 +0800

    2-first

--- test/src/cn/lala/main/TMain.java
+++ test/src/cn/lala/main/TMain.java
@@ -4,6 +4,6 @@ public class TMain {
 	public static void main(String[] args) {
 		System.out.println("ss");
 		System.out.println("good");
-		System.out.println("haha");
+		System.out.println("haha");sdf
 	}
 }
