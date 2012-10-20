3952d3c18578083bb6eebfe6270a80d62921bcc8
mawanjin <mwj7999@hotmail.com>
2012-10-20 15:33:48 +0800

    sdfdsa

--- test/src/cn/lala/main/TMain.java
+++ test/src/cn/lala/main/TMain.java
@@ -3,7 +3,7 @@ package cn.lala.main;
 public class TMain {
 	public static void main(String[] args) {
 
-		System.out.println("ss1");
-		System.out.println("1233");
+		System.out.println("jjj");
+		
 	}
 }
