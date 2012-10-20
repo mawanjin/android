e6e55944341f8622a832f68166d4f8f764d35a90
mawanjin <mwj7999@hotmail.com>
2012-10-20 14:23:35 +0800

    second

--- test/src/cn/lala/main/TMain.java
+++ test/src/cn/lala/main/TMain.java
@@ -3,5 +3,7 @@ package cn.lala.main;
 public class TMain {
 	public static void main(String[] args) {
 		System.out.println("ss");
+		System.out.println("good");
+		System.out.println("haha");
 	}
 }
