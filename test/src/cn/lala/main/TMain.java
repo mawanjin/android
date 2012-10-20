9945f2564186ca1086137b3f9cca2f45cc81f058
mawanjin <mwj7999@hotmail.com>
2012-10-20 15:24:05 +0800

    hhh

--- test/src/cn/lala/main/TMain.java
+++ test/src/cn/lala/main/TMain.java
@@ -3,7 +3,5 @@ package cn.lala.main;
 public class TMain {
 	public static void main(String[] args) {
 		System.out.println("1233");
-		System.out.println("ss1");
-		System.out.println("ss1");
 	}
 }
