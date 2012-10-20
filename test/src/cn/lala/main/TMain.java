f00df941eac9f3c49bf174ca931c639216c485f4
mawanjin <mwj7999@hotmail.com>
2012-10-20 15:36:41 +0800

    dsfa

--- test/src/cn/lala/main/TMain.java
+++ test/src/cn/lala/main/TMain.java
@@ -2,8 +2,6 @@ package cn.lala.main;
 
 public class TMain {
 	public static void main(String[] args) {
-		System.out.println("jjj");
-		System.out.println("ss1");
 		System.out.println("123");
 	}
 }
