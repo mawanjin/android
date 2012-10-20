71d135d92338c685865db9a45e4f85530552607c
mawanjin <mwj7999@hotmail.com>
2012-10-20 15:15:44 +0800

    dsfadsfs

--- test/src/cn/lala/main/TMain.java
+++ test/src/cn/lala/main/TMain.java
@@ -3,9 +3,5 @@ package cn.lala.main;
 public class TMain {
 	public static void main(String[] args) {
 		System.out.println("ss1");
-		System.out.println("good");
-		System.out.println("haha1");
-		System.out.println("haha1");
-		System.out.println("haha1");
 	}
 }
